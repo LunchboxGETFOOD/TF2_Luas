648c7ed95f1197aa2271a1027de4a1ba8f517c51
LunchboxGETFOOD <115940527+LunchboxGETFOOD@users.noreply.github.com>
2023-07-16 02:14:22 +0800

    Update AutoRecharge.lua
    add descriptions

--- AutoRecharge.lua
+++ AutoRecharge.lua
@@ -1,10 +1,10 @@
 local t = {}
 t.time = 0
-function dotimer(dt,interval,start)
+function dotimer(dt,interval,start) --delay
 	if start == true then
 		t.time = t.time + dt
 		if t.time >= interval then
-			t.time = t.time - interval    -- 重置计时器
+			t.time = t.time - interval    --reset timer
 			return true
 		else
 			return false
@@ -18,14 +18,13 @@ end
 callbacks.Register( 'CreateMove', function()
 	local localplayer = entities.GetLocalPlayer()
 	local charge = false
-	if warp.GetChargedTicks() ~= 23 then
+	if warp.GetChargedTicks() ~= 23 then --if cannot dt/warp
 		charge = true
-		if dotimer(0.16,10,charge) then
+		if dotimer(0.16,10,charge) then --default delay time is 10(ms), u can change to whatever u prefer (less than 10 may broke DT)
 			warp.TriggerCharge()
 		end
 	else
 		charge = false
 	end
-	print(charge)
 
 end)
